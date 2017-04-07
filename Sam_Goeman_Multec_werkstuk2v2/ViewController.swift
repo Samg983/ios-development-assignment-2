17173bfe1cb5a5568b7339152be9da33dff89038
Sam <sam.goeman@hotmail.com>
2017-04-07 14:55:35 +0200

    Initial Commit

---
+++ Sam_Goeman_Multec_werkstuk2v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sam_Goeman_Multec_werkstuk2v2
+//
+//  Created by Sam Goeman on 07/04/2017.
+//  Copyright © 2017 Sam Goeman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
